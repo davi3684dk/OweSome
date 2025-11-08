18a650e2483379254f809c0de25772c718ead81c
David H <davidhulstroem@gmail.com>
2025-11-08 13:55:40 +0100

    Create Group Screen and Navigation

--- app/src/main/java/com/owesome/data/entities/Group.kt
+++ app/src/main/java/com/owesome/data/entities/Group.kt
@@ -1,5 +1,5 @@
 package com.owesome.data.entities
 
-data class Group(val id: Int, val name: String, val description: String, val users: List<User>, val expenses: List<Expense>)
+data class Group(val id: Int, val name: String, val description: String, val users: List<User>, val expenses: List<Expense>, val status: Float)
 
 data class GroupCompact(val id: Int, val name: String, val description: String, val status: Number)
