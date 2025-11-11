69d7fcf117af38e91c7533809073566ced07ebb3
tubnielsen <tubnielsen@gmail.com>
2025-11-11 01:30:43 +0100

    login screen UI and navigation

--- app/src/main/java/com/owesome/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/owesome/ui/screens/RegisterScreen.kt
@@ -1,4 +1,14 @@
 package com.owesome.ui.screens
 
-class RegisterScreen {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(
+    navigation: NavController
+) {
+    Text("This is the register screen")
 }
