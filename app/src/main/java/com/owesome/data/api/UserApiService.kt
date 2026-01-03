4ef9c33a1e88c56cfc17aa305e3e5dec3bc414ae
zenrough <12938899-Zenrough@users.noreply.gitlab.com>
2026-01-03 16:46:44 +0100

    Added change password and change user settings api bindings to backend

--- app/src/main/java/com/owesome/data/api/UserApiService.kt
+++ app/src/main/java/com/owesome/data/api/UserApiService.kt
@@ -1,7 +1,7 @@
 package com.owesome.data.api
 
 import com.owesome.data.api.dto.UserDTO
-import com.owesome.data.entities.UserCreate
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
@@ -11,10 +11,17 @@ interface UserApiService {
     @GET("/users/{username}")
     suspend fun findUserByName(@Path("username") username: String): UserDTO?
 
+    // this returns a status and a message not a userDTO
+    @PATCH("/users/update/{id}/change-password")
+    suspend fun updateUserPassword(
+        @Path("id") id: Int,
+        @Body updateUserPasswordRequest: UpdateUserPasswordRequest
+    ) : Response<Unit>
+
     @PATCH("/users/update/{id}")
     suspend fun updateUserByID(
         @Path("id") id: Int,
-        @Body user: UserCreate
-    ): UserDTO?
+        @Body updateUserRequest : UpdateUserRequest
+    ) : Response<Unit>
 }
 
